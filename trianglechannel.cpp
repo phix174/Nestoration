b917981d6d2ad559e2b63ff1e0b7e78fdd0ce209
Don Laursen <gomtuu@gmail.com>
2020-07-18 16:14:06 -0400

    Made triangle cycle detection more accurate.

--- trianglechannel.cpp
+++ trianglechannel.cpp
@@ -9,28 +9,51 @@ TriangleChannel::TriangleChannel()
 }
 
 QVector<Cycle> TriangleChannel::runs_to_cycles(QList<Run> &runs) {
-    const samplesize LONGEST_RUN = 2048; // 32768 / 32
+    const samplesize LONGEST_CYCLE = 32768;
     QVector<Cycle> cycles;
+    if (runs.size() == 0) return cycles;
     const Cycle clear_cycle = { 0, CycleShape::Irregular, -999, {} };
-    for (int i=0; i < runs.size(); i += 1) {
-        // TODO: This should put up to 32 runs together in one cycle if they all share the same semitone id.
-        Cycle cycle = clear_cycle;
-        cycle.start = runs[i].start;
+    Cycle cycle = clear_cycle;
+    cycle.start = runs[0].start;
+    cycle.runs.append(runs[0]);
+    bool tip = (runs[0].value == 0 || runs[0].value == 15);
+    samplesize period = (tip ? 16 : 32) * runs[0].length;
+    if (period <= LONGEST_CYCLE) {
+        cycle.shape = CycleShape::Triangle;
+        cycle.semitone_id = period_to_semitone(period);
+    } else {
+        cycle.shape = CycleShape::None;
+    }
+    bool rising = true; // TODO: This assumption might cause a small problem?
+    bool prev_tip = tip;
+    samplesize prev_period = period;
+    bool prev_rising = rising;
+    for (int i=1; i < runs.size(); i += 1) {
+        tip = (runs[i].value == 0 || runs[i].value == 15);
+        period = (tip ? 16 : 32) * runs[i].length;
+        rising = (runs[i].value > runs[i-1].value);
+        bool changed_direction = (rising != prev_rising && !prev_tip);
+        bool changed_period = period != prev_period;
+        bool completed_cycle = cycle.runs.size() == 30;
+        if (changed_direction || changed_period || completed_cycle) {
+            //qDebug() << cycle.start << cycle.shape << cycle.semitone_id << cycle.runs.count() << sum_run_lengths(cycle);
+            cycles.append(cycle);
+            cycle = clear_cycle;
+            cycle.start = runs[i].start;
+        }
         cycle.runs.append(runs[i]);
-        if (runs[i].length < LONGEST_RUN) {
-            samplesize period = 16 * runs[i].length;
-            bool tip = (runs[i].value == 0 || runs[i].value == 15);
-            if (!tip) {
-                period *= 2;
-            }
+        if (period <= LONGEST_CYCLE) {
             cycle.shape = CycleShape::Triangle;
             cycle.semitone_id = period_to_semitone(period);
         } else {
             cycle.shape = CycleShape::None;
         }
-        cycles.append(cycle);
-        //qDebug() << cycle.start << cycle.shape << cycle.semitone_id << cycle.runs.count() << sum_run_lengths(cycle);
+        prev_tip = tip;
+        prev_period = period;
+        prev_rising = rising;
     }
+    //qDebug() << cycle.start << cycle.shape << cycle.semitone_id << cycle.runs.count() << sum_run_lengths(cycle);
+    cycles.append(cycle);
     qDebug() << "Cycle count: " << cycles.size();
     return cycles;
 }
