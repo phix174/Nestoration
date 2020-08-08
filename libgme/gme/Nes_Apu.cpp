phix174/Nestoration

commit 6893b291d952358644429f97bb9f24077a4a138d

Started adding support for length counter and sweep in tones.

libgme/gme/Nes_Apu.cpp
@@ -97,7 +97,7 @@ void Nes_Apu::reset( bool pal_mode, int initial_dmc_dac )
 	dmc.reset();
 	
 	last_time = 0;
-	total_frames = 0;
+	past_timeframe_cycles = 0;
 	apu_log.clear();
 	last_dmc_time = 0;
 	osc_enables = 0;
@@ -181,6 +181,7 @@ void Nes_Apu::run_until_( nes_time_t end_time )
 		
 		// take frame-specific actions
 		frame_delay = frame_period;
+		int old_square1_length_counter = square1.length_counter;
 		switch ( frame++ )
 		{
 			case 0:
@@ -195,6 +196,14 @@ void Nes_Apu::run_until_( nes_time_t end_time )
 				square2.clock_length( 0x20 );
 				noise.clock_length( 0x20 );
 				triangle.clock_length( 0x80 ); // different bit for halt flag on triangle
+				if ((square1.regs[0] & 0x20) && old_square1_length_counter > 0 && square1.length_counter <= 0) {
+					apu_log_t entry {
+						past_timeframe_cycles + time,
+						apu_log_event::timeout
+					};
+					entry.channel = 0;
+					apu_log.append(entry);
+				}
 				
 				square1.clock_sweep( -1 );
 				square2.clock_sweep( 0 );
@@ -239,8 +248,10 @@ inline void zero_apu_osc( T* osc, nes_time_t time )
 
 void Nes_Apu::end_frame( nes_time_t end_time )
 {
-	if ( end_time > last_time )
+	if ( end_time > last_time ) {
 		run_until_( end_time );
+		past_timeframe_cycles += end_time;
+	}
 	
 	if ( dmc.nonlinear )
 	{
@@ -271,7 +282,6 @@ void Nes_Apu::end_frame( nes_time_t end_time )
 		if ( earliest_irq_ < 0 )
 			earliest_irq_ = 0;
 	}
-	total_frames += 1;
 }
 
 // registers
@@ -292,7 +302,13 @@ void Nes_Apu::write_register( nes_time_t time, nes_addr_t addr, int data )
 	if ( unsigned (addr - start_addr) > end_addr - start_addr )
 		return;
 
-	apu_log.append({ total_frames * 89488 + time, addr, static_cast<char>(data)});
+	apu_log_t entry {
+		past_timeframe_cycles + time,
+		apu_log_event::register_write
+	};
+	entry.address = addr;
+	entry.data = static_cast<char>(data);
+	apu_log.append(entry);
 	
 	run_until_( time );
 	

libgme/gme/Nes_Apu.h
@@ -11,10 +11,17 @@
 typedef blargg_long nes_time_t; // CPU clock cycle count
 typedef unsigned nes_addr_t; // 16-bit memory address
 
+enum apu_log_event {
+	register_write,
+	timeout
+};
+
 struct apu_log_t {
 	long long cpu_cycle;
-	nes_addr_t address;
-	char data;
+	apu_log_event event;
+	nes_addr_t address = 0;
+	char data = 0;
+	char channel = 0;
 };
 
 #include "Nes_Oscs.h"
@@ -133,7 +140,9 @@ private:
 	int frame_period;
 	int frame_delay; // cycles until frame counter runs next
 	int frame; // current frame (0-3)
-	int total_frames;
+	// Reminder: the following timeframes are twentieths of a second, as dictated by Classic_Emu::set_sample_rate_
+	// and are not related the the APU's "frame counter".
+	int past_timeframe_cycles;
 	int osc_enables;
 	int frame_mode;
 	bool irq_flag;

src/nsfaudiofile.cpp
@@ -53,8 +53,20 @@ double TriangleRegisters::midi_note() {
 }
 
 bool MiniApu::write(short address, char data) {
-    if (0x4000 <= address && address < 0x4004)
-        return this->squares[0].write(address - 0x4000, data);
+    bool had_sweep = this->squares[0].sweep_enabled();
+    if (0x4000 <= address && address < 0x4004) {
+        this->squares[0].write(address - 0x4000, data);
+        bool has_sweep = this->squares[0].sweep_enabled();
+        if (!had_sweep && has_sweep) {
+            qDebug() << "Sweep Enabled"
+                     << this->squares[0].sweep_negate()
+                     << this->squares[0].sweep_shift()
+                     << this->squares[0].sweep_period();
+        }
+        if (had_sweep && !has_sweep) {
+            qDebug() << "Sweep Disabled";
+        }
+    }
     if (0x4004 <= address && address < 0x4008)
         return this->squares[1].write(address - 0x4004, data);
     if (0x4008 <= address && address < 0x400c)
@@ -63,6 +75,10 @@ bool MiniApu::write(short address, char data) {
         this->squares[0].enabled = data & 0x01;
         this->squares[1].enabled = data & 0x02;
         this->triangle.enabled = data & 0x04;
+    }
+    if (address == 0x4017) {
+        this->framecounter_mode = (data >> 7) & 0x01;
+        qDebug() << "Frame counter mode set to:" << QString::number(this->framecounter_mode);
     }
     return false;
 }
@@ -127,8 +143,16 @@ void NsfAudioFile::read_runs() {
     for (int channel_i = 0; channel_i < 3; channel_i += 1) {
         tone[channel_i].start = apu->apu_log.first().cpu_cycle;
     }
+    bool prev_sweep_enabled = miniapu.squares[0].sweep_enabled();
+    bool prev_sweep_negate = miniapu.squares[0].sweep_negate();
+    int prev_sweep_shift = miniapu.squares[0].sweep_shift();
+    int prev_sweep_period = miniapu.squares[0].sweep_period();
     for (const apu_log_t &entry: apu->apu_log) {
-        miniapu.write(entry.address, entry.data);
+        if (entry.event == apu_log_event::register_write) {
+            miniapu.write(entry.address, entry.data);
+        } else {
+            qDebug() << "Channel 0 timed out at" << entry.cpu_cycle;
+        }
         for (int channel_i = 0; channel_i < 3; channel_i += 1) {
             if (channel_i < 2) {
                 tone[channel_i].semitone_id = miniapu.squares[channel_i].midi_note();
@@ -154,14 +178,34 @@ void NsfAudioFile::read_runs() {
                         qDebug() << "Deleting length-0 tone";
                         tones[channel_i].removeLast();
                     } else {
-                        qDebug() << previous.start << previous.length << previous.semitone_id << previous.volume;
+                        //qDebug() << previous.start << previous.length << previous.semitone_id << previous.volume;
                     }
                     new_tone[channel_i] = true;
+                    if (channel_i == 0 && prev_sweep_enabled) {
+                        int half_frame = (miniapu.framecounter_mode == 1 ? 37282 : 29830) / 2;
+                        int sweep_steps = previous.length / half_frame;
+                        qDebug() << "Sweep steps:" << previous.length << "/" << half_frame;
+                        int start_nes_timer = previous.nes_timer;
+                        int end_nes_timer = start_nes_timer;
+                        for (int i=0; i<sweep_steps; i++) {
+                            int change_factor = end_nes_timer >> prev_sweep_shift;
+                            qDebug() << "Change factor:" << end_nes_timer << ">>" << prev_sweep_shift;
+                            if (prev_sweep_negate) {
+                                change_factor = -1 - change_factor;
+                            }
+                            end_nes_timer += change_factor;
+                        }
+                        qDebug() << "Sweep from" << start_nes_timer << "to" << end_nes_timer;
+                    }
                 }
             }
             if (new_tone[channel_i] || !has_previous[channel_i]) {
                 tones[channel_i].append(tone[channel_i]);
                 tone[channel_i] = ToneObject {};
+                prev_sweep_enabled = miniapu.squares[0].sweep_enabled();
+                prev_sweep_negate = miniapu.squares[0].sweep_negate();
+                prev_sweep_shift = miniapu.squares[0].sweep_shift();
+                prev_sweep_period = miniapu.squares[0].sweep_period();
                 has_previous[channel_i] = true;
                 new_tone[channel_i] = false;
             }

src/nsfaudiofile.h
@@ -47,6 +47,7 @@ class MiniApu {
 public:
     SquareRegisters squares[2];
     TriangleRegisters triangle;
+    char framecounter_mode;
     bool write(short address, char data);
 };
 

commit 8aa28b0f4836788eaf9d74c7c364735ff380d8e0

Fixed timing regression introduced in 6893b291d952358644429f97bb9f24077a4a138d. We need to count the elapsed time in the frame being ended, even if the APU was already up to date.

libgme/gme/Nes_Apu.cpp
@@ -257,10 +257,10 @@ inline void zero_apu_osc( T* osc, nes_time_t time )
 
 void Nes_Apu::end_frame( nes_time_t end_time )
 {
-	if ( end_time > last_time ) {
+	if ( end_time > last_time )
 		run_until_( end_time );
-		past_timeframe_cycles += end_time;
-	}
+	
+	past_timeframe_cycles += end_time;
 	
 	if ( dmc.nonlinear )
 	{